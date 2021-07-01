258ed20be7c3d0198cee93d198951f8444e45071
Isaac Woods <isaacwoods.home@gmail.com>
2021-07-01 02:18:56 +0100

    Make the viewport the correct size

--- app/src/components/scene.tsx
+++ app/src/components/scene.tsx
@@ -14,6 +14,7 @@ export default function Scene(props: Props) {
         <Container column auto>
             <Helmet>
                 <title>{props.title}</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1.0" />
             </Helmet>
             <Container auto>
                 <Sidebar />
