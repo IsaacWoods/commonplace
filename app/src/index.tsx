40f9fdd07d255bbd68f83c626e08e032e5c0a8c7
Isaac Woods <isaacwoods.home@gmail.com>
2021-07-01 02:31:05 +0100

    Add styled-components theme and global styling support
    Question: why on earth does `body` come with a margin by default?
    Answer: tbf, it's apparently so documents with no CSS don't look like
    complete ass. Fair enough.
    Solution: everyone adds this block to their CSS that removes that default.

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { GlobalStyle } from './theme';
+import { ThemeProvider } from 'styled-components';
 import Scene from './components/scene';
 
 const App = () =>
-    <Scene>
-        <h1>Hello, React!</h1>
-        <p>Content goes here</p>
-        <p>Content goes here</p>
-        <p>Content goes here</p>
-    </Scene>;
+    <ThemeProvider theme={{}}>
+        <GlobalStyle />
+        <Scene>
+            <h1>Hello, React!</h1>
+            <p>Content goes here</p>
+            <p>Content goes here</p>
+            <p>Content goes here</p>
+        </Scene>
+    </ThemeProvider>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
