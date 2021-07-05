13080b503846ff1191c745530165e9994d4184df
Isaac Woods <isaacwoods.home@gmail.com>
2021-07-05 15:32:02 +0100

    Normalize the global style and improve styling a little

--- app/src/theme.tsx
+++ app/src/theme.tsx
@@ -1,20 +1,44 @@
 import { createGlobalStyle } from 'styled-components';
+import normalizeStyle from 'styled-normalize';
 
 export const theme = {
     background: "#ffffff",
+    text: "#141419",
+
     sidebarWidth: 240,
     sidebarColor: "rgba(247, 246, 243)",
 
     headerDepth: 500,
 };
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle<{ theme: { text: string } }>`
+    ${normalizeStyle}
+
     * {
         box-sizing: border-box;
     }
 
-    html, body {
-        margin: 0;
-        padding: 0;
+    body {
+        font-family: -apply-system, sans-serif;
+        text-rendering: optimizeLegibility;
+        color: ${props => props.theme.text};
     }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-weight: 500;
+        line-height: 1.25;
+        margin-top: 1em;
+        margin-bottom: 0.5em;
+    }
+    h1 { font-size: 2.25em; }
+    h2 { font-size: 1.5em; }
+    h3 { font-size: 1.25em; }
+    h4 { font-size: 1em; }
+    h5 { font-size: 0.875em; }
+    h6 { font-size: 0.75em; }
 `;
