fbcccf3773e684323e7c241dcd1a6f49d851361b
Isaac Woods <isaacwoods.home@gmail.com>
2021-07-06 17:25:11 +0100

    Use Shift-Enter to extend the current list item

--- app/src/editor/nodes/list.ts
+++ app/src/editor/nodes/list.ts
@@ -21,6 +21,18 @@ export class ListItem extends Functionality {
     keys(schema) {
         return {
             "Enter": splitListItem(schema.nodes.list_item),
+            "Shift-Enter": (state, dispatch) => {
+                /*
+                 * When Shift-Enter is pressed, we extend the current list item, allowing multiple blocks to be
+                 * placed under a single item.
+                 */
+                if (!isInList(state) || !state.selection.empty) {
+                    return false;
+                }
+
+                if (dispatch) dispatch(state.tr.split(state.selection.to));
+                return true;
+            }
         }
     }
 }
@@ -43,4 +55,15 @@ export class List extends Functionality {
             wrappingInputRule(/^\s*([-*])\s*$/, schema.nodes.list),
         ];
     }
+}
+
+function isInList(state) {
+    for (let d = state.selection.$head.depth; d > 0; d--) {
+        const name = state.selection.$head.node(d).type.name;
+        if (name === "list") {
+            return true;
+        }
+    }
+
+    return false;
 }
