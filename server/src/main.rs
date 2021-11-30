1e9006a279be454ae347242df794f79b47afc209
Isaac Woods <isaacwoods.home@gmail.com>
2021-11-30 23:59:42 +0000

    Allow figment config to change dist path

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,25 +11,36 @@ use rocket::{
     Data,
     Request,
 };
-use std::{path::Path, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use zettel::Index;
 
 #[launch]
 pub fn rocket() -> _ {
+    let rocket = rocket::build();
+    let figment = rocket.figment();
+
     let index = Arc::new(Index::create());
     tokio::spawn(zettel::index::commit_index(index.clone()));
 
-    rocket::build()
+    let dist_dir = {
+        let dist_dir: Result<PathBuf, _> = figment.extract_inner("dist_dir");
+        dist_dir.unwrap_or_else(|_| PathBuf::from(relative!("../app/dist")))
+    };
+
+    rocket
         .manage(zettel::ZettelStore::new(index))
         .mount("/api", routes![zettel::create, zettel::fetch, zettel::list, zettel::search, zettel::update])
-        .mount("/static", FileServer::from(relative!("../app/dist")))
-        .mount("/", AppPage(Path::new(relative!("../app/dist/index.html"))))
+        .mount("/static", FileServer::from(&dist_dir))
+        .mount("/", AppPage(Path::new(&dist_dir).join("index.html")))
 }
 
 /// The app is routed on the client using `react-router-dom`, and so we need to serve `index.html` regardless of
 /// the actual path we're at. We do this at a high rank so that other routes are attempted first.
 #[derive(Clone)]
-struct AppPage(&'static Path);
+struct AppPage(PathBuf);
 
 impl Into<Vec<Route>> for AppPage {
     fn into(self) -> Vec<Route> {
