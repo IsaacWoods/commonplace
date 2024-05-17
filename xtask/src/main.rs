adb91005071a039b81f989267535b682b59fa296
Isaac Woods <isaacwoods.home@gmail.com>
2024-05-18 00:56:01 +0100

    Fix xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -12,11 +12,6 @@ fn main() -> Result<()> {
 
     let flags = flags::Task::from_env()?;
     match flags.subcommand {
-        flags::TaskCmd::Help(_) => {
-            println!("{}", flags::Task::HELP);
-            Ok(())
-        }
-
         flags::TaskCmd::Dist(dist) => {
             webpack_app(dist.prod)?;
             Ok(())
